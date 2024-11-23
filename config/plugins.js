219e463b1f08a9593b367d5e2fa02a257b07f3e3
y.glushkova <yanaglushkova08@gmail.com>
2024-11-23 23:07:17 +0400

    filter products by artist

--- config/plugins.js
+++ config/plugins.js
@@ -1,2 +1,13 @@
 module.exports = () => ({
+    meilisearch: {
+        config: {
+          host: "http://0.0.0.0:7700",
+          apiKey: "XyxlNWxIsyKAHZTtmSuL8v8Lc3ch3YbB5cim_e-WS54",
+          product: {
+            settings:  {
+              filterableAttributes: ['artist.id'],
+            }
+          },
+        }
+    }
 });
