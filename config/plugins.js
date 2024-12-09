33624c38a17ef60cf1641535c03f999941399ee4
y.glushkova <yanaglushkova08@gmail.com>
2024-12-09 14:49:45 +0300

    editable category.id field

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,7 @@ module.exports = () => ({
           apiKey: "XyxlNWxIsyKAHZTtmSuL8v8Lc3ch3YbB5cim_e-WS54",
           product: {
             settings:  {
-              filterableAttributes: ['artist.id'],
+              filterableAttributes: ['artist.id', 'category.id'],
             }
           },
         }
